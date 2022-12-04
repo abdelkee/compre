a3253a637ff983a6bed8665f4e5d98dfd2e82a73
abdelkee <abdell.anejjar@gmail.com>
2022-12-04 17:25:47 -0500

    supabase added

--- app/ProductsList.tsx
+++ app/ProductsList.tsx
@@ -3,6 +3,11 @@ import { ProductType } from "../types";
 import ProductCard from "./ProductCard";
 import { supabase } from "../utils/initSupabase";
 
+async function getProducts() {
+  const { data, error } = await supabase.from("products").select();
+  return { data, error };
+}
+
 function ProductsList() {
   return (
     <section className="grid grid-cols-2 gap-4">
