0b1c0938bb09ebbe40913e94b6ed9bb15ef600c5
abdelkee <abdell.anejjar@gmail.com>
2022-12-10 15:48:50 -0500

    cat supabase select done

--- app/cart/CartHeader.tsx
+++ app/cart/CartHeader.tsx
@@ -9,11 +9,11 @@ import { useSelector, useUser } from "../../context/ContextHook";
 const CartHeader = () => {
   const [totalPrice, setTotalPrice] = useState(12.2);
   const [totalQuantity, setTotalQuantity] = useState(4);
-  useEffect(() => {
-    supabase.rpc("get_priceSum").then((res) => {
-      console.log(res);
-    });
-  }, []);
+  // useEffect(() => {
+  //   supabase.rpc("get_priceSum").then((res) => {
+  //     console.log(res);
+  //   });
+  // }, []);
   //     if (orders === null || orders.length <= 0)
   //       return (
   //         <div className="grid pt-4 place-items-center">No orders yet !</div>
