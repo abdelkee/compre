74a881b3841beaec043b4f8c05e4328e37e5b73f
abdelkee <abdell.anejjar@gmail.com>
2022-12-13 23:06:48 -0500

    loading session interval done

--- app/shared/Wrapper.tsx
+++ app/shared/Wrapper.tsx
@@ -1,10 +1,20 @@
 "use client";
 import Link from "next/link";
-import { ReactNode, useEffect } from "react";
+import { ReactNode, useState, useEffect } from "react";
 import { useUser } from "../../context/ContextHook";
 
 function Wrapper({ children }: { children: ReactNode }) {
   const { session } = useUser();
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+  if (loading)
+    return (
+      <div className="w-full mt-40 grid place-items-center">Loading...</div>
+    );
   if (session === null)
     return (
       <main className="grid font-semibold text-purple-700 place-items-center">
