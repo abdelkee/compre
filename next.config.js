0237c00345e394cee7e7a8d051205b2f93a8f30c
abdelkee <abdell.anejjar@gmail.com>
2022-12-14 20:08:31 -0500

    pwa added

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const withPWA = require("next-pwa")({
+  dest: "public",
+});
+
+module.exports = withPWA({
   experimental: { appDir: true },
   images: {
     domains: ["exgqcqdbjjdllndxzmze.supabase.co"],
   },
-};
+});
+
+// module.exports = withPWA({
+//   pwa: {
+//     dest: "public",
+//     register: true,
+//     skipWaiting: true,
+//
+//   },
+// });
